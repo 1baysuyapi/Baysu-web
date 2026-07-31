a61be6a53c9909a8e391f5b36caa702e64b65197
Baysu Yapı <ebayrakci75@gmail.com>
2026-07-31 22:36:32 +0300

    Fix Bahce Ekipmanlari layout and remove old debug files

--- test_ie.ps1
+++
@@ -1,7 +0,0 @@
-$ie = New-Object -COM "InternetExplorer.Application"
-$ie.Visible = $false
-$path = (Resolve-Path "test_debug.html").Path
-$ie.Navigate("file:///$path")
-while ($ie.Busy -eq $true -or $ie.ReadyState -ne 4) { Start-Sleep -Milliseconds 100 }
-Write-Host $ie.Document.body.innerHTML
-$ie.Quit()
