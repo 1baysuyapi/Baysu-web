d004d155a0861697bdcdf98df15560994091679b
Baysu Yapı <ebayrakci75@gmail.com>
2026-08-05 13:26:52 +0300

    Add missing bahce-ekipmanlari to data.js

--- update_data.js
+++ update_data.js
@@ -4,11 +4,13 @@ let dataJs = fs.readFileSync('data.js', 'utf8');
 let finalHtml = fs.readFileSync('bahce_ekipmanlari_generated.html', 'utf8');
 const base64Encoded = Buffer.from(unescape(encodeURIComponent(finalHtml)), 'binary').toString('base64');
 
-const regex = /("bahce-ekipmanlari\.html"\s*:\s*")[^"]+(")/;
-if (regex.test(dataJs)) {
+if (dataJs.includes('"bahce-ekipmanlari.html"')) {
+    const regex = /("bahce-ekipmanlari\.html"\s*:\s*")[^"]+(")/;
     dataJs = dataJs.replace(regex, '$1' + base64Encoded + '$2');
-    fs.writeFileSync('data.js', dataJs, 'utf8');
-    console.log('data.js updated successfully!');
 } else {
-    console.log('Regex failed to match in data.js');
+    // Inject before the closing bracket
+    dataJs = dataJs.replace(/};\s*$/, '  "bahce-ekipmanlari.html": "' + base64Encoded + '",\n};');
 }
+
+fs.writeFileSync('data.js', dataJs, 'utf8');
+console.log('data.js updated successfully!');
